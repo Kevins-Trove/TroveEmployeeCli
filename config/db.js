cb15dc9a29702ba761c8ea42da460e756bc2d682
Kevin Roper <kevin@kevinstrove.com>
2024-05-06 16:40:45 -0700

    Fixed prompting logic and error catching.

--- config/db.js
+++ config/db.js
@@ -1,28 +1,156 @@
 const connection = require("./connection");
 const display = require("../lib/displayClass");
+const constants = require('../lib/constantsClass');
 
 class DB {
  
 
   // Functions
-
- 
-
   viewTable = function(name){
-    connection.promise().query("SELECT * FROM " + name)
+    let sql = '';
+    // give exception for different tables
+
+    if (name == constants.EMPLOYEE ){
+      sql = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.last_name , ', ', manager.first_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;";
+    } else if (name == constants.ROLE) {
+     sql = "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;";
+    } else  {
+     sql = "SELECT * FROM department;";
+    } 
+    
+    connection.promise().query(sql)
       .then( ([rows,fields]) => {
-        display.renderTableHeader("Roles", fields);
+        display.renderTableHeader(name, fields);
         display.renderTableFields(rows);
         display.renderTableFooter();
           
       })
-      .catch(console.log)
+      .catch((err) => console.log(err))
       .then( () => console.log("\n"));
   }
 
+  viewDepartmentBudgets = function(name){
+    connection.promise().query("SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;")
+      .then( ([rows,fields]) => {
+        display.renderTableHeader(name, fields);
+        display.renderTableFields(rows);
+        display.renderTableFooter();
+          
+      })
+      .catch((err) => console.log(err))
+      .then( () => console.log("\n"));
+  }
  
-}
+  employeesByDepartment = function(name){
+    connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id ;")
+      .then( ([rows,fields]) => {
+        display.renderTableHeader(name, fields);
+        display.renderTableFields(rows);
+        display.renderTableFooter();
+          
+      })
+      .catch((err) => console.log(err))
+      .then( () => console.log("\n"));
+  }
 
+  employeesByManager = function(name){
+    connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, CONCAT(manager.last_name , ', ', manager.first_name ) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id ORDER BY manager;")
+    .then( ([rows,fields]) => {
+      display.renderTableHeader(name, fields);
+      display.renderTableFields(rows);
+      display.renderTableFooter();
+        
+    })
+    .catch((err) => console.log(err))
+    .then( () => console.log("\n"));
+  }
+
+   // Find all roles, join with departments to display the department name
+   getDepartments() {
+    const result =  connection.promise().query(
+      "SELECT department.id, department.name FROM department;"
+    ).then( ([rows, fields]) => {
+
+      let roles = rows;
+      const roleChoices = roles.map(({ id, name }) => ({
+        name: `${id} - ${name}`,
+        value: `${id}`
+    }));
+      return roleChoices;
+        
+    })
+    .catch((err) => console.log(err))
+    .then( (choices) => choices);
+    
+    return result;
+  }
+   
+    // Find all managers
+    getManagers() {
+      const result =  connection.promise().query(
+        "SELECT employee.id, employee.first_name, employee.last_name FROM employee;"
+      ).then( ([rows, fields]) => {
+
+        let roles = rows;
+        const choices = roles.map(({ id, last_name, first_name }) => ({
+          name: `${id} - ${last_name}, ${first_name}`,
+          value: `${id}`
+        }));
+        return choices;
+          
+      })
+      .catch((err) => console.log(err))
+      .then( (choices) => choices);
+      
+      return result;
+    }
+  
+  
+    // Find all roles, join with departments to display the department name
+    getRoles() {
+      const result =  connection.promise().query(
+        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+      ).then( ([rows, fields]) => {
+
+        let roles = rows;
+        const roleChoices = roles.map(({ id, title }) => ({
+          name: `${id} - ${title}`,
+          value: `${id}`
+        }));
+        return roleChoices;
+          
+      })
+      .catch((err) => console.log(err))
+      .then( (choices) => choices);
+      
+      return result;
+    }
+     
+    createRole = function(role){
+      return connection.promise().query("INSERT INTO role SET ?", role);
+    }
+
+    createDepartment = function(department){
+      return connection.promise().query("INSERT INTO department SET ?", department);
+    }
+
+    createEmployee = function(employee){
+      return connection.promise().query("INSERT INTO employee SET ?", employee);
+    }
+
+    deleteRole = function(role){
+      return connection.promise().query("DELETE FROM role WHERE id = ?", role);
+    }
+
+    deleteDepartment = function(department){
+      return connection.promise().query("DELETE FROM department WHERE id = ?", department);
+    }
+
+    deleteEmployee = function(employee){
+      return connection.promise().query("DELETE FROM employee WHERE id = ?", employee);
+    }
+
+}
 
 
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { prompt } = require("inquirer");
+
 const display = require("./lib/displayClass");
 const question = require("./lib/questionClass");
 const constants = require('./lib/constantsClass');
@@ -11,45 +11,70 @@ init();
 async function init() {
   let action = '';
   
-    // Header
-    display.renderHeader();
-  
-    // Prompt loop
-    while (action !== constants.QUIT) {
-      processAction();
+  // Render header
+  display.renderHeader();
 
-      await delay(50);
+  // Prompt loop and view rendering
+  while (action !== constants.QUIT) {
+    processAction();
 
-      action = await getPrompt();
-    }
+    await delay(50);
 
+    action = await getPrompt();
+  }
 
+  // Exit footer
   display.renderFooter();
   process.exit();
+
 }
 
+// delay to make prompting work 
 function delay(milliseconds){
   return new Promise(resolve => {
       setTimeout(resolve, milliseconds);
   });
 }
-async function processAction() {
-    
-    
-       // Determine action to take
-       if (question.hasAction() && question.hasTable() && question.hasView()){   
-       
-    
-        
-      await db.viewTable(question.currentTable);
-      // clear view action
-      question.currentView = "";
 
+
+
+// render any action we have 
+async function processAction() {
+
+    // // View
+    // if (question.currentAction == constants.ADD && question.hasTable() ){   
+    //   switch (question.currentTable) {
+    //     case constants.ROLE:
+    //       addRole();
+    //       break;
+    //     default:
+
+    //     }
+    //   }
+
+  // View
+  if (question.hasAction() && question.hasTable() && question.hasView()){   
+    
+    switch (question.currentView) {
+      case constants.EMPLOYEE_BY_DEPARTMENT:
+        await db.employeesByDepartment(question.currentView);
+        break;
+      case constants.EMPLOYEE_BY_MANAGER:
+        await db.employeesByManager(question.currentView);
+        break;
+      case constants.BUDGET_BY_DEPARTMENT:
+        await db.viewDepartmentBudgets(question.currentView);
+        break;
+      default:
+        await db.viewTable(question.currentTable);
     }
-  
+
+    // clear view action
+    question.currentView = "";
+  }
  }
   
-
+// Prompt user for CLI inputs
 async function getPrompt() {
     let action = '';
 
@@ -64,12 +89,63 @@ async function getPrompt() {
       return await question.table();
     }
 
+    // Add functions
+    if (question.currentAction == constants.ADD ){
+      switch (question.currentTable){
+        case constants.ROLE:
+          result = await question.addRole();
+          question.currentAction = "";
+          return;
+          break;
+      case constants.DEPARTMENT:
+          result = await question.addDepartment();
+          question.currentAction = "";
+          return;
+          break;
+      case constants.EMPLOYEE:
+          result = await question.addEmployee();
+          question.currentAction = "";
+          return;
+          break;          
+      default:
+
+      }
+      
+    }
+    
+// Add functions
+if (question.currentAction == constants.DELETE ){
+  switch (question.currentTable){
+    case constants.ROLE:
+      result = await question.deleteRole();
+      question.currentAction = "";
+      return;
+      break;
+  case constants.DEPARTMENT:
+      result = await question.deleteDepartment();
+      question.currentAction = "";
+      return;
+      break;
+  case constants.EMPLOYEE:
+      result = await question.deleteEmployee();
+      question.currentAction = "";
+      return;
+      break;          
+  default:
+
+  }
+  
+}
     // Prompt for View of data
     if ( !question.hasView()){
         return await question.view();
     }
 
     
-    return constants.QUIT
+    return ;//constants.QUIT
  }
   
+ // Add functions
+ async function addRole(){
+   return await question.addRole();
+}
